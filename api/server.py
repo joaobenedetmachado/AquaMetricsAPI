af9edfd3365f4a5fab27c48d428954107854cbfd
joao machado <jvbm2177@gmail.com>
2024-10-07 19:05:32 -0300

    correcao da api

--- api/server.py
+++ api/server.py
@@ -1,33 +1,36 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+from pydantic import BaseModel
 
-# Conexão com MongoDB
 minhaSenha = '123'
 client = MongoClient(f"mongodb+srv://joaoteste:{minhaSenha}@iotcluster.a70ey.mongodb.net/?retryWrites=true&w=majority&appName=IotCluster")
 db = client["aqua"]
 collection = db["aquacollections"]
 
-# Criação da instância FastAPI
 app = FastAPI()
 
-# Rota para ler documentos do MongoDB
+class Documento(BaseModel):
+    data: str
+    ph: str
+    tds: str
+    temp: str
+
 @app.get("/documentos")
 async def ler_documentos():
     documentos = []
     cursor = collection.find({})
     for documento in cursor:
-        documento['_id'] = str(documento['_id'])  # Convertendo ObjectId para string
+        documento['_id'] = str(documento['_id'])  
         documentos.append(documento)
     return {"documentos": documentos}
 
-# Rota para inserir um novo documento no MongoDB
 @app.post("/documentos")
-async def inserir_documento(data: str, ph: str, tds: str, temp: str):
+async def inserir_documento(documento: Documento):
     novo_documento = {
-        "data": data,
-        "ph": ph,
-        "tds": tds,
-        "temp": temp
+        "data": documento.data,
+        "ph": documento.ph,
+        "tds": documento.tds,
+        "temp": documento.temp
     }
     resultado = collection.insert_one(novo_documento)
     return {"message": "Documento inserido", "id": str(resultado.inserted_id)}
