8a1284ec961fcead52a1cb682e7880c0e6afcba8
joao machado <jvbm2177@gmail.com>
2024-10-12 16:22:32 -0300

    ph, tds e temp tem que ser inteiros

--- api/server.py
+++ api/server.py
@@ -20,9 +20,9 @@ app.add_middleware(
 
 class Documento(BaseModel):
     data: str
-    ph: str  # string
-    tds: str  # string
-    temp: str  # string
+    ph: int  # string
+    tds: int # string
+    temp: int  # string
 
 @app.get("/documentos") # quando a rota for um get, ele faz isso:
 async def lerDocumentos():
